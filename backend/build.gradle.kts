636bc4d844f40511fe132472434fcb256ffe4cee
ryanhallock <89486399+ryanhallock@users.noreply.github.com>
2026-09-17 11:37:50 -0500

    backend: Add style and convert to libs.version.toml

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.1.1"
-	id("io.spring.dependency-management") version "1.1.7"
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.spotless)
 }
 
 group = "edu.utdallas.cs4485"
@@ -9,7 +10,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(25)
+		languageVersion = JavaLanguageVersion.of(libs.versions.java.get())
 	}
 }
 
@@ -18,22 +19,35 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-h2console")
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-data-jdbc-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(libs.bundles.spring.boot.starters)
+	implementation(libs.springdoc.openapi.starter.webmvc.ui)
+	developmentOnly(libs.spring.boot.devtools)
+	runtimeOnly(libs.h2)
+	annotationProcessor(libs.spring.boot.configuration.processor)
+	testImplementation(libs.bundles.spring.boot.test.starters)
+	testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+spotless {
+	java {
+		target("src/**/*.java")
+		palantirJavaFormat(libs.versions.palantir.java.format.get())
+		removeUnusedImports()
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
+	kotlinGradle {
+		target("*.gradle.kts")
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
+	format("misc") {
+		target("src/**/*.properties", "*.properties", "gradle/*.toml", ".gitattributes", ".gitignore")
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
 }
